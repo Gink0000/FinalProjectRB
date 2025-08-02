f7b54cae93a649776bcaea4cb03abb88d8935b97
RanvirBains <rbains34@mylangara.ca>
2025-08-01 19:57:02 -0700

    Update frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
